7e5813c681ac2c0a22b8043288f96e119774c330
Askhab Musaev <42musaev@gmail.com>
2020-08-21 20:57:34 +0300

    added Article model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField('Загаловок', max_length=256)
+    img = models.ImageField('Изоброжение', upload_to='article_img')
+    text = models.TextField('Текст')
+    date_pub = models.DateTimeField('Дата публкиации', default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
